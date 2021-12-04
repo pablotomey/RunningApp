73c7b8785639b435057fd18c9384d8840d7cc9f7
pablotomey <pablo.tp87@gmail.com>
2021-12-04 15:51:54 -0300

    The database is created and dagger hilt dependencies are implemented

--- app/src/main/java/com/konadev/runningapp/db/RunDao.kt
+++ app/src/main/java/com/konadev/runningapp/db/RunDao.kt
@@ -13,29 +13,29 @@ interface RunDao {
     suspend fun deleteRun(run: Run)
 
     @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
-    suspend fun getAllRunsSortedByDate(): LiveData<List<Run>>
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
 
     @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
-    suspend fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
+    fun getAllRunsSortedByTimeInMillis(): LiveData<List<Run>>
 
     @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
-    suspend fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
 
     @Query("SELECT * FROM running_table ORDER BY avgSpeedInKmh DESC")
-    suspend fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
 
     @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
-    suspend fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
 
     @Query("SELECT SUM(timeInMillis) FROM running_table")
-    suspend fun getTotalTimeInMillis(): LiveData<Long>
+    fun getTotalTimeInMillis(): LiveData<Long>
 
     @Query("SELECT SUM(caloriesBurned) FROM running_table")
-    suspend fun getTotalCaloriesBurned(): LiveData<Int>
+    fun getTotalCaloriesBurned(): LiveData<Int>
 
     @Query("SELECT SUM(distanceInMeters) FROM running_table")
-    suspend fun getTotalDistance(): LiveData<Int>
+    fun getTotalDistance(): LiveData<Int>
 
     @Query("SELECT SUM(avgSpeedInKmh) FROM running_table")
-    suspend fun getTotalAvgSpeed(): LiveData<Float>
+    fun getTotalAvgSpeed(): LiveData<Float>
 }
---
+++ app/src/main/java/com/konadev/runningapp/db/RunningDataBase.kt
@@ -0,0 +1,12 @@
+package com.konadev.runningapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Run::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class RunningDataBase: RoomDatabase() {
+
+    abstract fun runDao(): RunDao
+}
